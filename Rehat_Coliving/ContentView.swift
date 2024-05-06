0d3025907d4edd1ebd8b8f0607c5155df6c019d2
eva <evamartinas289@gmail.com>
2024-05-06 12:16:14 +0700

    commit eva

--- Rehat_Coliving/ContentView.swift
+++ Rehat_Coliving/ContentView.swift
@@ -15,6 +15,7 @@ struct ContentView: View {
                 .foregroundStyle(.tint)
             Text("Hello, world!")
             Text("wadidaw")
+            Text("punya eva")
             Image(systemName: "triangle")        }
         .padding()
     }
